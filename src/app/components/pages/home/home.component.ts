ae4eca5fcfa2f49b892385a90748e0aafe7fb811
Caio Vinícius Rodrigues Moreira <caiobiscas@gmail.com>
2025-04-06 22:25:01 -0300

    add google auth button and store token jwt

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { PaginatorState } from 'primeng/paginator';
 import { Subject, fromEvent, takeUntil } from 'rxjs';
 import { PageableResponseNfts } from 'src/app/interfaces/PageableResponseNfts';
 import { Nft } from 'src/app/interfaces/iNFT';
+import { IsLoggedService } from 'src/app/services/isLogged/is-logged.service';
 import { NftService } from 'src/app/services/nft/nft.service';
 
 @Component({
@@ -20,12 +22,20 @@ export class HomeComponent implements OnDestroy{
 
   pagina!: Params;
 
-  constructor(private nftService: NftService, private router: Router, private activatedRoute: ActivatedRoute){}
+  constructor(private nftService: NftService, private router: Router, private activatedRoute: ActivatedRoute, private messageService: MessageService, private isLoggedService: IsLoggedService){}
 
   ngOnInit(){
     this.activatedRoute.queryParams
     .pipe(takeUntil(this.destroy$))
     .subscribe(value => {
+      if(value['token']){
+        this.router.navigate(['']);
+        this.messageService.add({severity: 'success', summary: 'Autenticado', detail: 'Login realizado com sucesso!'});
+        localStorage.setItem("token", value['token']);
+
+        this.isLoggedService.setUsuarioLogado(value['token']);
+      }
+      console.log(value);
       this.pagina = value;
       this.fetchNfts(value);
       console.log('queryParams emitiu um valor: ', Object.entries(value))
@@ -59,20 +69,6 @@ export class HomeComponent implements OnDestroy{
       return;
     }
     this.router.navigate([''], {queryParams: {page: $event.page}});
-
-    // console.log(this.activatedRoute);
-    // this.fetchNfts(this.pagina);
-    // this.nftService.getAllPageable(this.pagina)
-    // .pipe(takeUntil(this.destroy$))
-    // .subscribe({
-    //   next: (response) => {
-    //     this.nfts = response.content;
-    //     this.pageableNfts = response;
-    //   },
-    //   error: (error) => {
-    //     console.log("Deu algum erro", error);
-    //   }
-    // });
   }
 
   ngOnDestroy(): void {
